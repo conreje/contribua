1af47a3ed8e287abb4db11d6659db1129f4eef8a
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2015-11-21 14:58:55 -0200

    Exibindo categorias e produtos.

--- js/doacoes.js
+++ js/doacoes.js
@@ -1,25 +1,141 @@
-(function(app, Marionette, $, _) {
-    function register(template) {
-        var Donnations = Marionette.ItemView.extend({
-            template: _.template(template),
+(function(app, Backbone, $, _) {
+    var Donation = Backbone.Model.extend({
+        defaults: {
+            name: '',
+            email: '',
+            phone: '',
+            products: []
+        },
+
+        add: function(product, category, quantity) {
+            this.get('products').push({
+                name: product.get('nome'),
+                category: category.get('nome'),
+                quantity: quantity,
+                metric: product.get('medida')
+            });
+        },
+
+        url: 'doacao.php'
+    });
+
+    var Product = Backbone.Model.extend({
+        initialize: function() {
+            this.listenTo(this, 'change:nome', this.configureSlug);
+            this.configureSlug();
+        },
+        
+        configureSlug: function() {
+            this.set('slug', $.slugify(this.get('nome')));
+        }
+    });
+
+    var Category = Backbone.Model.extend({
+        initialize: function() {
+            this.products = new ProductCollection({category: this});
+        },
+
+        getProducts: function() {
+            return this.products;
+        }
+    });
+
+    var ProductCollection = Backbone.Collection.extend({
+        model: Product,
+
+        initialize: function(options) {
+            this.category = options.category;
+        },
+
+        url: function() {
+            return this.category.get('produtos');
+        }
+    });
+
+    var CategoryCollection = Backbone.Collection.extend({
+        model: Category,
+        url: 'data/categorias.json'
+    });
+
+    function register(template, categoryTemplate, productTemplate) {
+        var categories = new CategoryCollection();
+        
+        var ProductView = Backbone.Marionette.ItemView.extend({
+            className: 'col s12 m6',
+            template: _.template(productTemplate[0])
+        });
+        
+        var ProductsView = Backbone.Marionette.CollectionView.extend({
+            childView: ProductView,
+            className: 'row'
+        });
+
+        var CategoryView = Backbone.Marionette.LayoutView.extend({
+            tagName: 'li',
+            template: _.template(categoryTemplate[0]),
+            
+            regions: {
+                'products': '[data-rel="produtos"]'
+            },
+
+            onBeforeShow: function() {
+                var products = this.model.getProducts();
+
+                products.fetch().then((function() {
+                    this.showChildView('products', new ProductsView({collection: products}));
+                }).bind(this));
+            }
+        });
+
+        var CategoriesView = Backbone.Marionette.CollectionView.extend({
+            childView: CategoryView,
+
+            tagName: 'ul',
+            className: 'collapsible popout produtos',
+            attributes: {
+                'data-collapsible': 'accordion'
+            },
+
+            onAttach: function() {
+                this.$el.collapsible();
+            }
+        });
+
+        var Donnations = Backbone.Marionette.LayoutView.extend({
+            template: _.template(template[0]),
+
+            regions: {
+                categories: '#categories'
+            },
             
             ui: {
                 back: '#back'
             },
-            
+
             events: {
                 'click @ui.back': 'back'
             },
-            
-            back: function() {
+
+            back: function(e) {
+                e.preventDefault();
+
                 this.triggerMethod('open', 'home')
+            },
+
+            onBeforeShow: function() {
+                this.showChildView('categories', new CategoriesView({collection: categories}));
             }
         });
         
         app.layout.registeredViews.donnations = Donnations;
+        categories.fetch();
     }
-    
+
     app.on('before:start', function() {
-        $.get('templates/doacoes.html').then(register);
+        $.when(
+            $.get('templates/doacoes.html'),
+            $.get('templates/categoria.html'),
+            $.get('templates/produto.html')
+        ).done(register);
     });
-})(Conreje.Contribua, Marionette, jQuery, _);
+})(Conreje.Contribua, Backbone, jQuery, _);
